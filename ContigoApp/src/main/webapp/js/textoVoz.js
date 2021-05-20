cc592df9f7ea2e3dcfb08d7725b3e2d668af979b
camilaFernandez <maria.fernandezo1@correo.usa.edu.co>
2021-05-20 13:56:41 -0500

    cc

--- ContigoApp/src/main/webapp/js/textoVoz.js
+++ ContigoApp/src/main/webapp/js/textoVoz.js
@@ -1,20 +1,22 @@
+
+var texto2= document.getElementById("texto2");
 document.getElementById('hablar').addEventListener("click", () => {
-    var texto=document.getElementById("texto").value;
+    /*var texto=document.getElementById("texto").value;
     var texto2= document.getElementById("texto2");
     console.log(texto2.innerHTML)
-    decir(texto);
-
-    /*speechSynthesis.onvoiceschanged = () => {
-        const text = 'Hola, soy Jorge Baumann y mi perro se llama Rambo'
+    decir(texto);*/
+    
+    speechSynthesis.onvoiceschanged = () => {
+        var text = texto2; 
         const synth = speechSynthesis
         const voices = synth.getVoices()
+        console.log(text)
         const utterThis = new SpeechSynthesisUtterance(text)
         utterThis.voice = voices.find(v => v.name === 'Jorge')
-        utterThis.pitch = 1.5
+        utterThis.pitch = 0
         utterThis.rate = 1
         synth.speak(utterThis)
-      }*/
-    
+      }
 });
 
 function decir(texto) {
